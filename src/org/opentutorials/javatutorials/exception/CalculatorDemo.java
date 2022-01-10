6190756691eca3a04b0ea040865a245340075daa
mdoli <mdoli@JHSHIN>
2022-01-11 01:42:47 +0900

    예외 1 - 문법

---
+++ src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -0,0 +1,34 @@
+package org.opentutorials.javatutorials.exception;
+
+class Calculator{
+    int left, right;
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+    public void divide(){
+        try {
+            System.out.print("계산결과는 ");
+            System.out.print(this.left/this.right);
+            System.out.print(" 입니다.");
+		} catch (Exception e) {
+			System.out.println("\n\ne.getMessage()\n"+e.getMessage());
+            System.out.println("\n\ne.toString()\n"+e.toString());
+            System.out.println("\n\ne.printStackTrace()");
+            e.printStackTrace();
+		}
+        System.out.println("Devide End");
+    }
+} 
+public class CalculatorDemo {
+    public static void main(String[] args) {
+        Calculator c1 = new Calculator();
+        c1.setOprands(10, 0);
+        c1.divide();
+        
+        Calculator c2 = new Calculator();
+        c2.setOprands(10, 5);
+        c2.divide();
+
+    }
+}
