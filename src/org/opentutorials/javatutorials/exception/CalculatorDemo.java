6768f3a4c640f75ba3cf3809b61f79c31349473f
shinjinhun <mamsorea@gmail.com>
2022-01-12 18:29:28 +0900

    업데이트

--- src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
+++ src/org/opentutorials/javatutorials/exception/CalculatorDemo.java
@@ -1,34 +1,55 @@
 package org.opentutorials.javatutorials.exception;
 
+class DivideException extends Exception {
+	public int left;
+	public int right;
+	
+	
+	DivideException() {
+		super();
+	}
+	DivideException(String message) {
+		super(message);
+	}
+	
+	DivideException(String message, int left, int right) {
+		super(message);
+        this.left = left;
+        this.right = right;		
+	}	
+}
+
 class Calculator{
     int left, right;
     public void setOprands(int left, int right){
         this.left = left;
         this.right = right;
     }
-    public void divide(){
-        try {
-            System.out.print("계산결과는 ");
-            System.out.print(this.left/this.right);
-            System.out.print(" 입니다.");
-		} catch (Exception e) {
-			System.out.println("\n\ne.getMessage()\n"+e.getMessage());
-            System.out.println("\n\ne.toString()\n"+e.toString());
-            System.out.println("\n\ne.printStackTrace()");
-            e.printStackTrace();
-		}
-        System.out.println("Devide End");
+    public void divide() throws DivideException{
+    	
+    	if(right == 0) {
+    		throw new DivideException("0으로 나눌 수 없습니다. ", this.left, this.right);
+    	}
+    	System.out.print(this.left/this.right);
+        
     }
 } 
 public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
         c1.setOprands(10, 0);
-        c1.divide();
+        try {
+        	c1.divide();	
+		} catch (DivideException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.left);
+			System.out.println(e.right);
+		}
         
-        Calculator c2 = new Calculator();
-        c2.setOprands(10, 5);
-        c2.divide();
+        
+//        Calculator c2 = new Calculator();
+//        c2.setOprands(10, 5);
+//        c2.divide();
 
     }
 }
